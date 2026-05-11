0ba4f059f9d9a5620d980a74864ef03ba1d671a2
Rohan Shinge <rohanshinge0528@gmail.com>
2026-05-11 20:56:45 +0530

    Update full_adder_tb.v

--- full_adder_tb.v
+++ full_adder_tb.v
@@ -1,24 +1,22 @@
-`timescale 1ns / 1ps
-
 module full_adder_tb;
 
-reg a, b, cin;
-wire sum, carry;
-
-full_adder dut(a, b, cin, sum, carry);
-
-initial begin
-    $monitor("Time=%0t | a=%b b=%b cin=%b | sum=%b carry=%b",
-              $time, a, b, cin, sum, carry);
+    reg a, b, cin;
+    wire sum, carry;
     
-    a=0; b=0; cin=0; #10;
-    a=0; b=1; cin=0; #10;
-    a=1; b=0; cin=0; #10;
-    a=1; b=1; cin=0; #10;
-    a=0; b=0; cin=1; #10;
-    a=1; b=1; cin=1; #10;
+    full_adder uut(a, b, cin, sum, carry);
 
-    $finish;
-end
+    initial 
+        begin
+        a=0; b=0; cin=0; #10;
+        a=0; b=1; cin=0; #10;
+        a=1; b=0; cin=0; #10;
+        a=1; b=1; cin=0; #10;
+        a=0; b=0; cin=1; #10;
+        a=0; b=1; cin=1; #10;
+        a=1; b=0; cin=1; #10;
+        a=1; b=1; cin=1; #10;
+
+        $stop;
+    end
 
 endmodule
